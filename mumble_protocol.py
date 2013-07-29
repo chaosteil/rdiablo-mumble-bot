3aba7ead9c1a24d8bba855dff86aa139ef2c35a2
Dominykas Djačenka <Chaosteil@gmail.com>
2013-07-29 15:52:42 +0300

    Added license to mumble_protocol.py

--- mumble_protocol.py
+++ mumble_protocol.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+###
+# Chaosteil 2013
+#
+# This work has been released into the public domain by the authors. This
+# applies worldwide.
+#
+# If this is not legally possible, the authors grant any entity the right to
+# use this work for any purpose, without any conditions, except those conditions
+# required by law.
+###
+
 import logging
 import optparse
 import platform
